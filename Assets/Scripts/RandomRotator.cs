c20e7fadae84145d63109a008ff8619bcab5462c
Danny <Danny>
2018-06-18 18:26:48 +0900

    061818_1

---
+++ Assets/Scripts/RandomRotator.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomRotator : MonoBehaviour {
+	public GameObject explosion;
+	public GameObject playerExplosion;
+	public float tumble;
+
+	void OnTriggerEnter(Collider other){
+		if (other.tag == "Boundary"){
+			return;
+		}
+		Instantiate(explosion, transform.position, transform.rotation);
+		if (other.tag == "Player"){
+			Instantiate(playerExplosion, other.transform.position, other.transform.rotation);
+		}
+		Destroy (other.gameObject);
+		Destroy (gameObject);
+	}
+
+	void Start (){
+		rigidbody.angularVelocity = Random.insideUnitSphere * tumble;
+	}
+}
