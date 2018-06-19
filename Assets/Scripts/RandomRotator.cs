94258f833c6fcb36e5817672b8f17656a86a6ff2
Danny <Danny>
2018-06-19 18:15:28 +0900

    Tuto fini

--- Assets/Scripts/RandomRotator.cs
+++ Assets/Scripts/RandomRotator.cs
@@ -5,6 +5,20 @@ public class RandomRotator : MonoBehaviour {
 	public GameObject explosion;
 	public GameObject playerExplosion;
 	public float tumble;
+	public int scoreValue;
+	private GameController gameController;
+
+	void Start(){
+		rigidbody.angularVelocity = Random.insideUnitSphere * tumble;
+
+		GameObject gameControllerObject = GameObject.FindGameObjectWithTag("GameController");
+		if (gameControllerObject != null){
+			gameController = gameControllerObject.GetComponent<GameController>();
+		}
+		if (gameController == null){
+			Debug.Log("Cannot find 'Game Controller' script");
+		}
+	}
 
 	void OnTriggerEnter(Collider other){
 		if (other.tag == "Boundary"){
@@ -13,12 +27,10 @@ public class RandomRotator : MonoBehaviour {
 		Instantiate(explosion, transform.position, transform.rotation);
 		if (other.tag == "Player"){
 			Instantiate(playerExplosion, other.transform.position, other.transform.rotation);
+			gameController.GameOver();
 		}
+		gameController.AddScore(scoreValue);
 		Destroy (other.gameObject);
 		Destroy (gameObject);
-	}
-
-	void Start (){
-		rigidbody.angularVelocity = Random.insideUnitSphere * tumble;
 	}
 }
